2179f8f61251e76bc423ba9803a991474ecb3b68
Spartak BF <job.spartak@gmail.com>
2021-04-16 20:19:56 -0400

    removed some features

--- app.js
+++ app.js
@@ -45,19 +45,11 @@ function generatorColors(val){
     COLORS.push(...first, ...second);
 }
 
-// here is a helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
 function shuffle(array) {
   let counter = array.length;
-  // While there are elements in the array
   while (counter > 0) {
-    // Pick a random index
     let index = Math.floor(Math.random() * counter);
-    // Decrease counter by 1
     counter--;
-
-    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
@@ -79,33 +71,17 @@ function shuffle(array) {
   return array;
 }
 
-
-//let shuffledColors = shuffle(COLORS);
-
-// this function loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
-    // create a new div
     const newDiv = document.createElement("div");
-
-    // give it a class attribute for the value we are looping over
     newDiv.classList.add(color);
-
-    // call a function handleCardClick when a div is clicked on
     newDiv.addEventListener("click", handleCardClick);
-
-    // append the div to the element with an id of game
     gameContainer.append(newDiv);
   }
 }
 
 
-// TODO: Implement this function!
 function handleCardClick(e) {
-  // you can use event.target to see which element was clicked
-  //console.log("you just clicked", event.target);
 
   let i = 0;
   let tagDiv;
@@ -165,13 +141,6 @@ function getCurrentScore(){
 }
 
 function removeTopHead(){
-  //let span = document.querySelector("#currentScore");
-    //let score = parseInt(span.innerText)+1;
-      //span.innerText = score;
-      //if(parseInt(localStorage.getItem("bestScore")) < score){
-        //localStorage.setItem("bestScore", score);
-        //document.querySelector("#bestScore").innerHTML = score;
-      //}
   let divs = document.querySelectorAll('#game div');
   divs.forEach(div => {
     if( div.classList.contains("top-head") && !div.classList.contains("match")){
